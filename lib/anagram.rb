91ee7c42b594996c33434d5b24a51761784a43be
Richard Sanchez <richard.charles.s@gmail.com>
2018-07-28 16:21:02 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,18 +6,18 @@ class Anagram
     @word = word
   end
 
-  # def match(array)
-  #   result = []
-  #   array.each do |array_word|
-  #     if @word.length == array_word.length
-  #       if sort_letters(@word) == sort_letters(array_word)
-  #         result << array_word
-  #       end
-  #     end
-  #   end
-  #   result
-  # end
-  #
+  def match(array)
+    result = []
+    array.each do |array_word|
+      if @word.length == array_word.length
+        if sort_letters(@word) == sort_letters(array_word)
+          result << array_word
+        end
+      end
+    end
+    result
+  end
+
   # def sort_letters(word)
   #   word.chars.sort.join
   # end
